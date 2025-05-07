7ef89a807c053cd0f1222601e7c73c7772b61ef7
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-05-07 16:40:23 +0600

    add-button

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -185,7 +185,7 @@ function CarouselPrevious({
       variant={variant}
       size={size}
       className={cn(
-        "absolute size-8 shadow-md rounded-full border border-[#E6E6E8] dark:border-gray-700 ",
+        "absolute size-8 shadow-md rounded-full border border-[#E6E6E8]  ",
         orientation === "horizontal"
           ? "top-1/2 -left-5 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -195,7 +195,7 @@ function CarouselPrevious({
       onClick={scrollPrev}
       {...props}
     >
-      <MdChevronLeft className="h-4 w-4" />
+      <MdChevronLeft className="h-4 w-4 text-gray-700" />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
@@ -215,7 +215,7 @@ function CarouselNext({
       variant={variant}
       size={size}
       className={cn(
-        "absolute size-8 shadow-md rounded-full border border-[#E6E6E8] dark:border-gray-700",
+        "absolute size-8 shadow-md rounded-full border border-[#E6E6E8] ",
         orientation === "horizontal"
           ? "top-1/2 -right-5 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -225,7 +225,7 @@ function CarouselNext({
       onClick={scrollNext}
       {...props}
     >
-      <MdChevronRight className="h-4 w-4" />
+      <MdChevronRight className="h-4 w-4 text-gray-700" />
       <span className="sr-only">Next slide</span>
     </Button>
   );
